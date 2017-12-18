ad8bbf3cbf36138a20cbf28ad45cac0edaf234a2
NakanishiJun <>
2017-12-18 17:14:45 +0900

    fix for to upto

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,21 +1,11 @@
-def fizz_buzz
- 
-	for num in 1...101 do
-
-		if num % 15 == 0 then
-	 		puts 'fizzbuzz'
-	 
-		elsif num % 3 == 0 then
-			puts 'fizz'
-
-		elsif num % 5 == 0 then
-			puts 'buzz' 
-
-		else
-			puts num
-		end	
-	end
-
-end
-
-fizz_buzz
+1.upto(100){|num|
+    if num % 15 == 0 then
+        puts 'fizzbuzz'
+    elsif num % 3 == 0 then
+        puts 'fizz'
+    elsif num % 5 == 0 then	
+        puts 'buzz' 
+    else
+        puts num
+    end	
+}
