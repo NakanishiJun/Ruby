c60ad37842670cb890cbcf694ac8c90e1ad658b9
NakanishiJun <>
2017-12-20 17:59:52 +0900

    delete then

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,9 +1,9 @@
 1.upto(100){|num|
-    if num % 15 == 0 then
+    if num % 15 == 0
         puts 'fizzbuzz'
-    elsif num % 3 == 0 then
+    elsif num % 3 == 0 
         puts 'fizz'
-    elsif num % 5 == 0 then	
+    elsif num % 5 == 0	
         puts 'buzz' 
     else
         puts num
